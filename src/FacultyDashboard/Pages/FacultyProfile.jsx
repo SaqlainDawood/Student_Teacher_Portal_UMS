496373adab9cbd5c64cd95391efff266c0d36f59
Saqlain Dawood <saqlaindawood123@gmail.com>
2026-02-09 01:13:55 -0800

    update facultly...

--- src/FacultyDashboard/Pages/FacultyProfile.jsx
+++ src/FacultyDashboard/Pages/FacultyProfile.jsx
@@ -45,7 +45,18 @@ export default function FacultyProfile() {
         }
         fetchtFacultyProfile();
   },[navigate])
-   if (loading) return <div>Loading faculty data...</div>;
+  if (loading) {
+    return (
+      <div className="approvals-container">
+        <div className="container-fluid text-center py-5">
+          <div className="spinner-border text-primary" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </div>
+          <p className="mt-3">Loading pending approvals...</p>
+        </div>
+      </div>
+    );
+  }
 
   return (
    <>
