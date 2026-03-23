345245a3ae9011cd3e6998478037537472b7034a
Saqlain Dawood <saqlaindawood123@gmail.com>
2026-03-22 17:37:17 -0700

    Updated authContext

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,7 @@ import App from './App.jsx'
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "react-toastify/dist/ReactToastify.css";
-import { AuthProvider } from './FacultyDashboard/Context/AuthContext.js';
-
+import { AuthProvider } from './FacultyDashboard/Context/AuthContext.jsx'
 createRoot(document.getElementById('root')).render(
 
   <StrictMode>
