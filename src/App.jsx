6348ea96dbbbf9d165d2af58b01171dae0fe1a66
Saqlain Dawood <saqlaindawood123@gmail.com>
2026-03-29 21:32:53 -0700

    Update Student Dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -47,7 +47,6 @@ function App() {
             <Route path="result" element={<StudentResult />} />
             <Route path="notes" element={<StudentNotes />} />
             <Route path="attendance" element={<StudentAttendance />} />
-            <Route path="attendance" element={<StudentAttendance />} />
           </Route>
                {/* Facult Dashbaord */}
            
